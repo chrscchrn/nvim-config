c5ad809bb0bf76ba16e72f2d9d0fadb45cf6557b
christopher cochran <cochran@ibm.com>
2026-04-17 16:02:39 -0500

    test - add osc52 for dell only

--- init.lua
+++ init.lua
@@ -1,12 +1,34 @@
 require 'config.config'
 
-vim.opt.runtimepath:prepend("/home/chris/.local/share/nvim/site/")
+vim.opt.runtimepath:prepend '/home/chris/.local/share/nvim/site/'
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then error('Error cloning lazy.nvim:\n' .. out) end
+end
+
+local handle = io.popen 'hostnamectl --value Hardware\\ Vendor'
+local vendor = handle:read('*a'):gsub('^%s*(.-)%s*$', '%1') -- Trims spaces from both ends
+handle:close()
+
+-- This will show you exactly what Fedora is reporting
+print('System Vendor: ' .. vendor)
+
+if vendor == 'Dell Inc.' or vendor == 'DellInc.' then
+  print 'Action: Applying Dell-specific settings.'
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+      ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+      ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+    },
+  }
 end
 
 ---@type vim.Option
@@ -30,7 +52,6 @@ require('lazy').setup({
   require 'cc.treesitter',
   require 'cc.trouble',
   require 'cc.whichkey',
-
 }, {
   ui = {
     icons = vim.g.have_nerd_font and {} or {
